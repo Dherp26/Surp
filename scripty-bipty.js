3508f897d4a6b29e66b2491f89f3ac3e6cbd7e00
Dherp26 <61307952+Dherp26@users.noreply.github.com>
2024-12-04 17:28:22 +0700

    Add files via upload

---
+++ scripty-bipty.js
@@ -0,0 +1,247 @@
+var canvas = document.getElementById("starfield");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var context = canvas.getContext("2d");
+var stars = 500;
+var colorrange = [0, 60, 240];
+var starArray = [];
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Initialize stars with random opacity values
+for (var i = 0; i < stars; i++) {
+    var x = Math.random() * canvas.offsetWidth;
+    var y = Math.random() * canvas.offsetHeight;
+    var radius = Math.random() * 1.2;
+    var hue = colorrange[getRandom(0, colorrange.length - 1)];
+    var sat = getRandom(50, 100);
+    var opacity = Math.random();
+    starArray.push({ x, y, radius, hue, sat, opacity });
+}
+
+var frameNumber = 0;
+var opacity = 0;
+var secondOpacity = 0;
+var thirdOpacity = 0;
+
+var baseFrame = context.getImageData(0, 0, window.innerWidth, window.innerHeight);
+
+function drawStars() {
+    for (var i = 0; i < stars; i++) {
+        var star = starArray[i];
+
+        context.beginPath();
+        context.arc(star.x, star.y, star.radius, 0, 360);
+        context.fillStyle = "hsla(" + star.hue + ", " + star.sat + "%, 88%, " + star.opacity + ")";
+        context.fill();
+    }
+}
+
+function updateStars() {
+    for (var i = 0; i < stars; i++) {
+        if (Math.random() > 0.99) {
+            starArray[i].opacity = Math.random();
+        }
+    }
+}
+
+const button = document.getElementById("valentinesButton");
+
+button.addEventListener("click", () => {
+//   if (button.textContent === "Click Me! ❤") {
+//     button.textContent = "loading...";
+//     fetch('send_mail.php')
+//       .then(response => {
+//         if (response.ok) {
+//           button.textContent = "More love for you❤";
+//         } else {
+//           console.error('Failed to send email');
+//           button.textContent = "Error 😞";
+//         }
+//       })
+//       .catch(error => {
+//         // Handle network errors or other issues
+//         console.error('Error:', error);
+//         button.textContent = "Error 😞";
+//       });
+//   }
+});
+
+function drawTextWithLineBreaks(lines, x, y, fontSize, lineHeight) {
+    lines.forEach((line, index) => {
+        context.fillText(line, x, y + index * (fontSize + lineHeight));
+    });
+}
+
+function drawText() {
+    var fontSize = Math.min(30, window.innerWidth / 24); // Adjust font size based on screen width
+    var lineHeight = 8;
+
+    context.font = fontSize + "px Comic Sans MS";
+    context.textAlign = "center";
+    
+    // glow effect
+    context.shadowColor = "rgba(45, 45, 255, 1)";
+    context.shadowBlur = 8;
+    context.shadowOffsetX = 0;
+    context.shadowOffsetY = 0;
+
+    if(frameNumber < 250){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+        context.fillText("每天我都無法相信自己有多麼幸運", canvas.width/2, canvas.height/2);
+        opacity = opacity + 0.01;
+    }
+    //fades out the text by decreasing the opacity
+    if(frameNumber >= 250 && frameNumber < 500){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+        context.fillText("每天我都無法相信自己有多麼幸運", canvas.width/2, canvas.height/2);
+        opacity = opacity - 0.01;
+    }
+
+    //needs this if statement to reset the opacity before next statement on canvas
+    if(frameNumber == 500){
+        opacity = 0;
+    }
+    if(frameNumber > 500 && frameNumber < 750){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+
+        if (window.innerWidth < 600) {           //shortens long sentence for mobile screens
+            drawTextWithLineBreaks(["在數以兆計的星星之中,", "經歷了數十億年"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+        } else {
+            context.fillText("在數以兆計的星星之中，經歷了數十億年", canvas.width/2, canvas.height/2);
+        }
+
+        opacity = opacity + 0.01;
+    }
+    if(frameNumber >= 750 && frameNumber < 1000){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+        
+        if (window.innerWidth < 600) {
+            drawTextWithLineBreaks(["在數以兆計的星星之中,", "經歷了數十億年"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+        } else {
+            context.fillText("在數以兆計的星星之中, 經歷了數十億年", canvas.width/2, canvas.height/2);
+        }
+
+        opacity = opacity - 0.01;
+    }
+
+    if(frameNumber == 1000){
+        opacity = 0;
+    }
+    if(frameNumber > 1000 && frameNumber < 1250){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+        context.fillText("能夠活著，並且能與你一起度過這一生", canvas.width/2, canvas.height/2);
+        opacity = opacity + 0.01;
+    }
+    if(frameNumber >= 1250 && frameNumber < 1500){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+        context.fillText("能夠活著，並且能與你一起度過這一生", canvas.width/2, canvas.height/2);
+        opacity = opacity - 0.01;
+    }
+
+    if(frameNumber == 1500){
+        opacity = 0;
+    }
+    if(frameNumber > 1500 && frameNumber < 1750){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+        context.fillText("我愛你，真的愛你，崇拜你，並且非常珍惜你，對你充滿感激", canvas.width/2, canvas.height/2);
+        opacity = opacity + 0.01;
+    }
+    if(frameNumber >= 1750 && frameNumber < 2000){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+        context.fillText("我愛你，真的愛你，崇拜你，並且非常珍惜你，對你充滿感激", canvas.width/2, canvas.height/2);
+        opacity = opacity - 0.01;
+    }
+
+    if(frameNumber == 2000){
+        opacity = 0;
+    }
+    if(frameNumber > 2000 && frameNumber < 2250){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+
+        if (window.innerWidth < 600) {
+            drawTextWithLineBreaks(["而在這裡的我", "會照顧你直到我們生命的盡頭"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+        } else {
+            context.fillText("而在這裡的我會照顧你直到我們生命的盡頭", canvas.width/2, canvas.height/2);
+        }
+
+        opacity = opacity + 0.01;
+    }
+    if(frameNumber >= 2250 && frameNumber < 2500){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+
+        if (window.innerWidth < 600) {
+            drawTextWithLineBreaks(["而在這裡的我", "會照顧你直到我們生命的盡頭"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+        } else {
+            context.fillText("而在這裡的我會照顧你直到我們生命的盡頭", canvas.width/2, canvas.height/2);
+        }
+        
+        opacity = opacity - 0.01;
+    }
+
+    if(frameNumber == 2500){
+        opacity = 0;
+    }
+    if(frameNumber > 2500 && frameNumber < 99999){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+
+        if (window.innerWidth < 600) {
+            drawTextWithLineBreaks(["我愛你如此深", "超過宇宙中所有的時間和空間所能容納的"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+        } else {
+            context.fillText("我愛你如此深，超過宇宙中所有的時間和空間所能容納的", canvas.width/2, canvas.height/2);
+        }
+
+        opacity = opacity + 0.01;
+    }
+    
+    if(frameNumber >= 2750 && frameNumber < 99999){
+        context.fillStyle = `rgba(45, 45, 255, ${secondOpacity})`;
+
+
+        if (window.innerWidth < 600) {
+            drawTextWithLineBreaks(["我迫不及待想與你共度所有時間", "將我的愛與你分享!"], canvas.width / 2, (canvas.height/2 + 60), fontSize, lineHeight);
+        } else {
+            context.fillText("我迫不及待想與你共度所有時間，將我的愛與你分享!", canvas.width/2, (canvas.height/2 + 50));
+        }
+
+        secondOpacity = secondOpacity + 0.01;
+    }
+
+    if(frameNumber >= 3000 && frameNumber < 99999){
+        context.fillStyle = `rgba(45, 45, 255, ${thirdOpacity})`;
+        context.fillText("I really really love you❤", canvas.width/2, (canvas.height/2 + 120));
+        thirdOpacity = thirdOpacity + 0.01;
+
+        button.style.display = "block";
+    }   
+
+     // Reset the shadow effect after drawing the text
+     context.shadowColor = "transparent";
+     context.shadowBlur = 0;
+     context.shadowOffsetX = 0;
+     context.shadowOffsetY = 0;
+}
+
+function draw() {
+    context.putImageData(baseFrame, 0, 0);
+
+    drawStars();
+    updateStars();
+    drawText();
+
+    if (frameNumber < 99999) {
+        frameNumber++;
+    }
+    window.requestAnimationFrame(draw);
+}
+
+window.addEventListener("resize", function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    baseFrame = context.getImageData(0, 0, window.innerWidth, window.innerHeight);
+});
+
+window.requestAnimationFrame(draw);
